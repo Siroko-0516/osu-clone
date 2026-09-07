5ef134a4143057116fdb71428e7339c15db4308f
Siroko-0516 <jisung.20090725@gmail.com>
2026-09-08 07:46:03 +0900

    Add maintainable browser storage foundation

--- osu.Web/Program.cs
+++ osu.Web/Program.cs
@@ -1,8 +1,11 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using osu.Web;
+using osu.Game.Database.Storage;
+using osu.Web.Storage;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
+builder.Services.AddScoped<IRecordStore, IndexedDbRecordStore>();
 await builder.Build().RunAsync();
