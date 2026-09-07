110b0680d4392f900865da1542bf5e0c264754a2
Siroko-0516 <jisung.20090725@gmail.com>
2026-09-07 09:44:25 +0900

    web: fix WebAssembly host namespace

--- osu.Web/Program.cs
+++ osu.Web/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using osu.Web;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
