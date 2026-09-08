0261d1236e867802899b1ecef16067126d0e1662
Siroko-0516 <jisung.20090725@gmail.com>
2026-09-08 11:56:07 +0900

    Add first browser-rendered scene geometry

--- osu.Web/BrowserBootstrapGame.cs
+++ osu.Web/BrowserBootstrapGame.cs
@@ -1,6 +1,11 @@
 // Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+using osu.Framework.Allocation;
+using osu.Framework.Graphics;
+using osu.Framework.Graphics.Shapes;
+using osuTK.Graphics;
+
 namespace osu.Web
 {
     /// <summary>
@@ -9,5 +14,24 @@ namespace osu.Web
     /// </summary>
     public sealed class BrowserBootstrapGame : osu.Framework.Game
     {
+        [BackgroundDependencyLoader]
+        private void load()
+        {
+            Children = new Drawable[]
+            {
+                new Box
+                {
+                    RelativeSizeAxes = Axes.Both,
+                    Colour = new Color4(18, 12, 24, 255),
+                },
+                new Box
+                {
+                    Anchor = Anchor.Centre,
+                    Origin = Anchor.Centre,
+                    Size = new osuTK.Vector2(360, 120),
+                    Colour = new Color4(236, 52, 123, 255),
+                },
+            };
+        }
     }
 }
