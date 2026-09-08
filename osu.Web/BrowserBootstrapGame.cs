a3101c828a059f3be0c607736dd0ea57ede65e26
Siroko-0516 <jisung.20090725@gmail.com>
2026-09-08 10:24:41 +0900

    Disambiguate framework Game type

--- osu.Web/BrowserBootstrapGame.cs
+++ osu.Web/BrowserBootstrapGame.cs
@@ -1,15 +1,13 @@
 // Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
-using osu.Framework;
-
 namespace osu.Web
 {
     /// <summary>
     /// Realm-free browser runtime used while persistent osu! services are migrated to IndexedDB.
     /// Keeping this host separate ensures unsupported native database code can never run in WebAssembly.
     /// </summary>
-    public sealed class BrowserBootstrapGame : Game
+    public sealed class BrowserBootstrapGame : osu.Framework.Game
     {
     }
 }
