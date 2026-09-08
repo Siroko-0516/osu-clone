70c9bea24f7dacdc5a8115d01111176ea4d492b5
Siroko-0516 <jisung.20090725@gmail.com>
2026-09-08 09:49:43 +0900

    Satisfy web host code quality rules

--- osu.Web/Program.cs
+++ osu.Web/Program.cs
@@ -1,11 +1,22 @@
+// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
+// See the LICENCE file in the repository root for full licence text.
+
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using osu.Web;
 using osu.Game.Database.Persistence;
 using osu.Web.Storage;
 
-var builder = WebAssemblyHostBuilder.CreateDefault(args);
-builder.RootComponents.Add<App>("#app");
-builder.RootComponents.Add<HeadOutlet>("head::after");
-builder.Services.AddScoped<IRecordStore, IndexedDbRecordStore>();
-await builder.Build().RunAsync();
+namespace osu.Web
+{
+    public static class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            builder.RootComponents.Add<App>("#app");
+            builder.RootComponents.Add<HeadOutlet>("head::after");
+            builder.Services.AddScoped<IRecordStore, IndexedDbRecordStore>();
+            await builder.Build().RunAsync();
+        }
+    }
+}
