f1e494a85e484118192e8e1727d44934661db760
Siroko-0516 <jisung.20090725@gmail.com>
2026-09-10 08:43:02 +0900

    Seed browser persistence with built-in skins

--- osu.Game/Database/Persistence/GamePersistence.cs
+++ osu.Game/Database/Persistence/GamePersistence.cs
@@ -3,8 +3,10 @@
 
 #nullable enable
 
+using System.Linq;
 using System.Threading.Tasks;
 using osu.Game.Configuration;
+using osu.Game.Skinning;
 
 namespace osu.Game.Database.Persistence
 {
@@ -41,8 +43,29 @@ namespace osu.Game.Database.Persistence
         {
             var beatmaps = await BeatmapCatalogStore.CreateAsync(records).ConfigureAwait(false);
             var skins = await GameCollectionStore<SkinSnapshot>.CreateAsync(records, "skins").ConfigureAwait(false);
+            await ensureBuiltInSkinsPresent(skins).ConfigureAwait(false);
             var scores = await GameCollectionStore<ScoreSnapshot>.CreateAsync(records, "scores").ConfigureAwait(false);
             return new GamePersistence(records, rulesetSettings, beatmaps, skins, scores);
+        }
+
+        private static async Task ensureBuiltInSkinsPresent(GameCollectionStore<SkinSnapshot> skins)
+        {
+            var builtInSkins = new[]
+            {
+                new SkinSnapshot(SkinInfo.ARGON_SKIN.ToString(), "osu! argon", string.Empty, true),
+                new SkinSnapshot(SkinInfo.ARGON_PRO_SKIN.ToString(), "osu! argon pro", string.Empty, true),
+                new SkinSnapshot(SkinInfo.TRIANGLES_SKIN.ToString(), "osu!triangles", string.Empty, true),
+                new SkinSnapshot(SkinInfo.CLASSIC_SKIN.ToString(), "osu! default", string.Empty, true),
+                new SkinSnapshot(SkinInfo.RETRO_SKIN.ToString(), "osu!classic", string.Empty, true),
+            };
+
+            foreach (SkinSnapshot skin in builtInSkins)
+            {
+                if (skins.Values.Any(existing => existing.Id == skin.Id))
+                    continue;
+
+                await skins.SaveAsync(skin.Id, skin).ConfigureAwait(false);
+            }
         }
     }
 }
